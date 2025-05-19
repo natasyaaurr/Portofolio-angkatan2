90ff1697f87cbdd2e94140de49a09c9a1827871b
rizkilabs <rizkilabs.dev@gmail.com>
2025-05-19 11:02:40 +0700

    First Commit

---
+++ admin/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form | Portofolio Natasyah</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+<body>
+<div class="wrapper">
+    <div class="login-form mt-5">
+        <div class="container">
+            <div class="row  justify-content-center">
+                <div class="col-sm-6">
+                    <div class="card">
+                    <div class="card-header"> Login Form</div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="mb-3">
+                                <label for="" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Ex:admin@gmail.com" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Masukkan Password Anda.." required>
+                            </div>
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary" name="login">Login</button>
+                                </div>
+                        </form>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>   
+</body>
+</html>
